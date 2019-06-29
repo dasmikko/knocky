10e7395f6d089d7955548254be51e2bf6e6f7b5e
dasmikko <dasmikko@gmail.com>
2019-06-29 09:30:37 +0200

    refactor api

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -5,51 +5,61 @@ import 'package:knocky/models/subforumDetails.dart';
 import 'package:knocky/models/thread.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-const bool isDev = false;
+class KnockoutAPI {
+  static bool _isDev;
 
-const baseurlSite = !isDev ? "https://knockout.chat/" : "https://forums.stylepunch.club/";
-const baseurl = !isDev ? "https://api.knockout.chat/" : "https://forums.stylepunch.club:3000/";
+  String _baseurlSite =
+      !_isDev ? "https://knockout.chat/" : "https://forums.stylepunch.club/";
+  String _baseurl = !_isDev
+      ? "https://api.knockout.chat/"
+      : "https://forums.stylepunch.club:3000/";
 
-List<Subforum> parseSubforums(String responseBody) {
-  final parsed = json.decode(responseBody)['list'].cast<Map<String, dynamic>>();
+  List<Subforum> parseSubforums(String responseBody) {
+    final parsed =
+        json.decode(responseBody)['list'].cast<Map<String, dynamic>>();
 
-  return parsed.map<Subforum>((json) => Subforum.fromJson(json)).toList();
+    return parsed.map<Subforum>((json) => Subforum.fromJson(json)).toList();
+  }
+
+  Future<List<Subforum>> getSubforums() async {
+    final response = await http.get(_baseurl + 'subforum');
+
+    return parseSubforums(response.body);
+  }
+
+  Future<SubforumDetails> getSubforumDetails(int id) async {
+    final response = await http.get(_baseurl + 'subforum/' + id.toString());
+
+    return SubforumDetails.fromJson(json.decode(response.body));
+  }
+
+  Future<Thread> getThread(int id, {int page: 1}) async {
+    final response = await http
+        .get(_baseurl + 'thread/' + id.toString() + '/' + page.toString());
+
+    return Thread.fromJson(json.decode(response.body));
+  }
+
+  Future<Thread> authCheck() async {
+    print('Checking auth state...');
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final response = await http.get(_baseurl + 'user/authCheck',
+        headers: {'Cookie': prefs.getString('cookieString')});
+
+    print('Auth: ' + response.body);
+  }
+
+  Future<void> getAlerts() async {
+    final response = await http.get(_baseurl + 'alert/list', headers: {});
+
+    print(response.body);
+  }
+
+  Future<void> getEvetns() async {
+    final response = await http.get(_baseurl + 'events', headers: {});
+
+    print(response.body);
+  }
 }
-
-Future<List<Subforum>> getSubforums () async {
-  final response =
-        await http.get(baseurl + 'subforum');
-
-  return parseSubforums(response.body);
-}
-
-Future<SubforumDetails> getSubforumDetails (int id) async {
-  final response =
-        await http.get(baseurl + 'subforum/' + id.toString());
-
-
-  return SubforumDetails.fromJson(json.decode(response.body));
-}
-
-Future<Thread> getThread (int id, {int page: 1}) async {
-  final response =
-        await http.get(baseurl + 'thread/' + id.toString() + '/' + page.toString());
-
-
-  return Thread.fromJson(json.decode(response.body));
-}
-
-Future<Thread> authCheck () async {
-  print('Checking auth state...');
-
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-
-  final response =
-        await http.get(baseurl + 'user/authCheck', headers: {
-          'Cookie': prefs.getString('cookieString')
-        });
-
-
-  print('Auth: ' + response.body);
-}
-
