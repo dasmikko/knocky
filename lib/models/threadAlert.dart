01298e38890154cda8b5e1cb055b16089e81094e
dasmikko <dasmikko@gmail.com>
2019-06-29 09:30:47 +0200

    Start on alerts

---
+++ lib/models/threadAlert.dart
@@ -0,0 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class ThreadAlert {
+  @JsonKey(name: 'avatar_url')
+  final String avatarUrl;
+  @JsonKey(name: 'background_url')  
+  final String backgroundUrl;
+  final String email;
+  final int firstUnreadId;
+  @JsonKey(name: 'icon_id')
+  final int iconId;
+  final int id;
+  @JsonKey(name: 'last_seen')
+  final DateTime lastSeen;
+  final int locked;
+  final int pinned;
+  @JsonKey(name: 'subforum_id')
+  final int subforumId;
+  final DateTime threadCreatedAt;
+  @JsonKey(nullable: true)
+  final DateTime threadDeletedAt;
+  final String threadTitle;
+  final DateTime threadUpdateAt;
+  final int threadUser;
+
+
+}
