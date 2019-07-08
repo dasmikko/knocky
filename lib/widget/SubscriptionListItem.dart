eaa2a9909bce76405c9ca267d773a90e7ddca896
dasmikko <dasmikko@gmail.com>
2019-07-08 08:45:47 +0200

    Added handler for new posts button and fixed some styling

---
+++ lib/widget/SubscriptionListItem.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:knocky/models/threadAlert.dart';
+import 'package:knocky/helpers/icons.dart';
+import 'package:knocky/widget/InkWellOnWidget.dart';
+
+class SubscriptionListItem extends StatelessWidget {
+  ThreadAlert item;
+  Function onTapItem;
+  Function onTapNewPostButton;
+
+  SubscriptionListItem({this.item, this.onTapItem, this.onTapNewPostButton});
+
+  @override
+  Widget build(BuildContext context) {
+    String iconUrl = iconList.where((icon) => icon.id == item.iconId).first.url;
+
+    return Card(
+      color: Color.fromRGBO(45, 45, 48, 1),
+      clipBehavior: Clip.antiAlias,
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CachedNetworkImage(
+                    width: 25,
+                    imageUrl: iconUrl,
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              child: Container(
+                color: Color.fromRGBO(34, 34, 38, 1),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () => onTapItem(item),
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(bottom: 5),
+                            child: Text(item.threadTitle),
+                          ),
+                          if (item.unreadPosts > 0)
+                            Stack(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 5),
+                                  child: ClipRRect(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(5),
+                                      ),
+                                      clipBehavior: Clip.antiAlias,
+                                      child: InkWellOverWidget(
+                                        child: Container(
+                                          padding: EdgeInsets.all(5),
+                                          color:
+                                              Color.fromRGBO(255, 201, 63, 1),
+                                          child: Text(
+                                            '${item.unreadPosts} new posts',
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          onTapNewPostButton(item);
+                                        },
+                                      )),
+                                ),
+                              ],
+                            ),
+                          Text(
+                            item.threadUsername,
+                            style: TextStyle(color: Colors.blue),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 130,
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 4),
+                    child: Text(
+                        item.lastPost.thread.postCount.toString() + ' replies'),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      timeago.format(item.createdAt),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 4),
+                    child: Text(
+                      item.lastPost.user.username,
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 4),
+                    child: Text(timeago.format(item.lastPost.createdAt)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
