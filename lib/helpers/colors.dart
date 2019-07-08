3e9c58aeb4385592f7188120cfd87ce5100c96f2
dasmikko <dasmikko@gmail.com>
2019-07-08 22:25:00 +0200

    Make Color class for dynamic colors

--- lib/helpers/colors.dart
+++ lib/helpers/colors.dart
@@ -1,3 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+
 class AppColors {
-  
+  BuildContext context;
+  Brightness currentBrightness;
+
+  AppColors(BuildContext context) {
+    this.context = context;
+    this.currentBrightness = DynamicTheme.of(this.context).brightness;
+  }
+
+  Color userQuoteBodyBackground() {
+    return this.currentBrightness == Brightness.dark
+        ? HexColor('2d2d30')
+        : HexColor('e6e6e6');
+  }
+
+  Color userQuoteHeaderBackground() {
+    return this.currentBrightness == Brightness.dark
+        ? HexColor('28282c')
+        : HexColor('ebedef');
+  }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
