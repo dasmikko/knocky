671b6b6d2370cfbfdc71a57cd1981e5759304322
dasmikko <dasmikko@gmail.com>
2019-07-08 22:25:34 +0200

    Improve dark theme

--- lib/themes/DarkTheme.dart
+++ lib/themes/DarkTheme.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:knocky/helpers/colors.dart';
 
 ThemeData DarkTheme() {
+  Color userQuoteBody = HexColor('#2d2d30');
+
   return ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.red,
+    scaffoldBackgroundColor: HexColor('#111015'),
+    cardColor: HexColor('#222226')
   );
 }
