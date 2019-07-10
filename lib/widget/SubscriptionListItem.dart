398bb3e3f783880fe5c08a5cb4a4f1c3a862b95f
dasmikko <dasmikko@gmail.com>
2019-07-10 22:18:40 +0200

    Start on unsubscribe feature

--- lib/widget/SubscriptionListItem.dart
+++ lib/widget/SubscriptionListItem.dart
@@ -6,6 +6,7 @@ import 'package:knocky/helpers/icons.dart';
 import 'package:knocky/widget/InkWellOnWidget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:knocky/helpers/colors.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class SubscriptionListItem extends StatelessWidget {
   ThreadAlert item;
@@ -21,141 +22,158 @@ class SubscriptionListItem extends StatelessWidget {
     return Card(
       color: Color.fromRGBO(45, 45, 48, 1),
       clipBehavior: Clip.antiAlias,
-      child: IntrinsicHeight(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CachedNetworkImage(
-                    width: 25,
-                    imageUrl: iconUrl,
-                  ),
-                ],
+      child: Slidable(
+        actionPane: SlidableDrawerActionPane(),
+        actionExtentRatio: 0.25,
+        secondaryActions: <Widget>[
+          IconSlideAction(
+            caption: 'Unsubscribe',
+            color: Colors.red,
+            icon: FontAwesomeIcons.trash,
+            onTap: () => print('archive'),
+          ),
+        ],
+        child: IntrinsicHeight(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CachedNetworkImage(
+                      width: 25,
+                      imageUrl: iconUrl,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Flexible(
-              child: Container(
-                color: Color.fromRGBO(34, 34, 38, 1),
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    onTap: () => onTapItem(item),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(bottom: 5),
-                            child: RichText(
-                              text: TextSpan(children: <InlineSpan>[
-                                if (item.locked == 1)
-                                  WidgetSpan(
-                                    child: Container(
-                                      margin: EdgeInsets.only(right: 5),
-                                      child: Icon(
-                                        FontAwesomeIcons.lock,
-                                        size: 14,
-                                        color: HexColor('b38d4f'),
-                                      ),
-                                    ),
-                                  ),
-                                if (item.pinned == 1)
-                                  WidgetSpan(
-                                    alignment: PlaceholderAlignment.bottom,
-                                    child: Container(
-                                      margin: EdgeInsets.only(right: 5),
-                                      child: Icon(
-                                        FontAwesomeIcons.solidStickyNote,
-                                        size: 14,
-                                        color: HexColor('b4e42d'),
-                                      ),
-                                    ),
-                                  ),
-                                TextSpan(
-                                  text: item.title,
-                                ),
-                              ]),
-                            ),
-                          ),
-                          if (item.unreadPosts > 0)
-                            Stack(
-                              children: <Widget>[
-                                Container(
-                                  margin: EdgeInsets.only(bottom: 5),
-                                  child: ClipRRect(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(5),
-                                      ),
-                                      clipBehavior: Clip.antiAlias,
-                                      child: InkWellOverWidget(
-                                        child: Container(
-                                          padding: EdgeInsets.all(5),
-                                          color:
-                                              Color.fromRGBO(255, 201, 63, 1),
-                                          child: Text(
-                                            '${item.unreadPosts} new posts',
-                                            style:
-                                                TextStyle(color: Colors.black),
-                                          ),
+              Flexible(
+                child: Container(
+                  color: Color.fromRGBO(34, 34, 38, 1),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () => onTapItem(item),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(bottom: 5),
+                              child: RichText(
+                                text: TextSpan(children: <InlineSpan>[
+                                  if (item.locked == 1)
+                                    WidgetSpan(
+                                      child: Container(
+                                        margin: EdgeInsets.only(right: 5),
+                                        child: Icon(
+                                          FontAwesomeIcons.lock,
+                                          size: 14,
+                                          color: HexColor('b38d4f'),
                                         ),
-                                        onTap: () {
-                                          onTapNewPostButton(item);
-                                        },
-                                      )),
-                                ),
-                              ],
+                                      ),
+                                    ),
+                                  if (item.pinned == 1)
+                                    WidgetSpan(
+                                      alignment: PlaceholderAlignment.bottom,
+                                      child: Container(
+                                        margin: EdgeInsets.only(right: 5),
+                                        child: Icon(
+                                          FontAwesomeIcons.solidStickyNote,
+                                          size: 14,
+                                          color: HexColor('b4e42d'),
+                                        ),
+                                      ),
+                                    ),
+                                  TextSpan(
+                                    text: item.title,
+                                  ),
+                                ]),
+                              ),
                             ),
-                          Text(
-                            item.threadUsername,
-                            style: TextStyle(color: Colors.blue),
-                          )
-                        ],
+                            if (item.unreadPosts > 0)
+                              Stack(
+                                children: <Widget>[
+                                  Container(
+                                    margin: EdgeInsets.only(bottom: 5),
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(5),
+                                        ),
+                                        clipBehavior: Clip.antiAlias,
+                                        child: InkWellOverWidget(
+                                          child: Container(
+                                            padding: EdgeInsets.all(5),
+                                            color:
+                                                Color.fromRGBO(255, 201, 63, 1),
+                                            child: Text(
+                                              '${item.unreadPosts} new posts',
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            ),
+                                          ),
+                                          onTap: () {
+                                            onTapNewPostButton(item);
+                                          },
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            Text(
+                              item.threadUsername,
+                              style: TextStyle(color: Colors.blue),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              width: 130,
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(bottom: 4),
-                    child: Text(
-                        item.lastPost.thread.postCount.toString() + ' replies', style: TextStyle(fontSize: 11),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 4),
-                    child: Text(
-                      timeago.format(item.createdAt),
-                      style: TextStyle(fontSize: 11),
+              Container(
+                width: 130,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 4),
+                      child: Text(
+                        item.lastPost.thread.postCount.toString() + ' replies',
+                        style: TextStyle(fontSize: 11),
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 4),
-                    child: Text(
-                      item.lastPost.user.username,
-                      style: TextStyle(color: Colors.blue, fontSize: 11),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 4),
+                      child: Text(
+                        timeago.format(item.createdAt),
+                        style: TextStyle(fontSize: 11),
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 4),
-                    child: Text(timeago.format(item.lastPost.createdAt), style: TextStyle(fontSize: 11),),
-                  ),
-                ],
+                    Container(
+                      margin: EdgeInsets.only(bottom: 4),
+                      child: Text(
+                        item.lastPost.user.username,
+                        style: TextStyle(color: Colors.blue, fontSize: 11),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 4),
+                      child: Text(
+                        timeago.format(item.lastPost.createdAt),
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
