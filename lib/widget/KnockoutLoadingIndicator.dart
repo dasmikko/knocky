5c881e467bb6273b6ccd7d3d3b001fa4d5cff377
dasmikko <dasmikko@gmail.com>
2019-07-19 10:14:43 +0200

    Add fade transition

--- lib/widget/KnockoutLoadingIndicator.dart
+++ lib/widget/KnockoutLoadingIndicator.dart
@@ -1,25 +1,79 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class KnockoutLoadingIndicator extends StatelessWidget {
+class KnockoutLoadingIndicator extends StatefulWidget {
+  final Widget child;
+  bool show = false;
+
+  KnockoutLoadingIndicator({this.child, this.show});
+
+  @override
+  _KnockoutLoadingIndicatorState createState() =>
+      _KnockoutLoadingIndicatorState();
+}
+
+class _KnockoutLoadingIndicatorState extends State<KnockoutLoadingIndicator>
+    with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 100), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    /*animation.addStatusListener((status) {
+    if (status == AnimationStatus.completed) {
+      controller.reverse();
+    } else if (status == AnimationStatus.dismissed) {
+      controller.forward();
+    }
+  });*/
+//this will start the animation
+  
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 12),
-              child: Text(
-                'Node graph out of date. Rebuilding...',
-                style: TextStyle(fontSize: 14, fontFamily: 'RobotoMono'),
+    if (this.widget.show) {
+      controller.forward();
+    } else {
+      controller.reverse();
+    }
+
+    return Stack(
+      children: <Widget>[
+        this.widget.child,
+        Positioned(
+          child: FadeTransition(
+            opacity: animation,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          'Node graph out of date. Rebuilding...',
+                          style:
+                              TextStyle(fontSize: 14, fontFamily: 'RobotoMono'),
+                        ),
+                      ),
+                      CircularProgressIndicator()
+                    ],
+                  ),
+                ),
               ),
             ),
-            CircularProgressIndicator()
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
