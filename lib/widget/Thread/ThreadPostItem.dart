eedbf376f1187600c4af25ae195e78f87df656cb
dasmikko <dasmikko@gmail.com>
2019-07-29 08:17:15 +0200

    Disable view ratings if there are none

--- lib/widget/Thread/ThreadPostItem.dart
+++ lib/widget/Thread/ThreadPostItem.dart
@@ -15,9 +15,16 @@ class ThreadPostItem extends StatelessWidget {
   final GlobalKey scaffoldKey;
   final Function onPostRated;
   final Function onPressReply;
+  final Function onLongPressReply;
+  final bool isOnReplyList;
 
-
-  ThreadPostItem({this.postDetails, this.scaffoldKey, this.onPostRated, this.onPressReply});
+  ThreadPostItem(
+      {this.postDetails,
+      this.scaffoldKey,
+      this.onPostRated,
+      this.onPressReply,
+      this.isOnReplyList = false,
+      this.onLongPressReply});
 
   Widget buildRatings(List<ThreadPostRatings> ratings) {
     List<Widget> items = List();
@@ -140,15 +147,25 @@ class ThreadPostItem extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    if(postDetails.bans != null)
-                      Row(children: postDetails.bans.map((ban) => Flexible(child: PostBan(ban: ban,),),).toList(),),
+                    if (postDetails.bans != null)
+                      Row(
+                        children: postDetails.bans
+                            .map(
+                              (ban) => Flexible(
+                                child: PostBan(
+                                  ban: ban,
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Flexible(
                           child: FlatButton(
                             padding: EdgeInsets.all(0),
-                            onPressed: () => onPressViewRatings(context),
+                            onPressed: postDetails.ratings != null ? () => onPressViewRatings(context) : null,
                             child: buildRatings(postDetails.ratings),
                           ),
                         ),
@@ -158,10 +175,16 @@ class ThreadPostItem extends StatelessWidget {
                             onPressed: () => onPressRatePost(context),
                           ),
                         if (isLoggedIn && postDetails.user.id != ownUserId)
-                          FlatButton(
-                            child: Text('Reply'),
-                            onPressed: () => onPressReply(postDetails),
-                          )
+                          GestureDetector(
+                            onLongPress: () => onLongPressReply(postDetails),
+                            child: FlatButton(
+                              child: Text(
+                                  !this.isOnReplyList ? 'Reply' : 'Unreply'),
+                              onPressed: () => onPressReply(postDetails),
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 0),
+                            ),
+                          ),
                       ],
                     ),
                   ],
