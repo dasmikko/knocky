155d252f858a3b90d723dcd333ff085b41a71d3c
dasmikko <dasmikko@gmail.com>
2019-08-16 22:34:04 +0200

    Fix download helper

--- lib/helpers/Download.dart
+++ lib/helpers/Download.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:path_provider_ex/path_provider_ex.dart';
 import 'package:path/path.dart';
 import 'dart:io';
 
 class DownloadHelper {
-  void downloadFile(String url, GlobalKey<ScaffoldState> scaffoldKey) {
+  void downloadFile(String url, GlobalKey<ScaffoldState> scaffoldKey) async {
+    List<StorageInfo> dir = await PathProviderEx.getStorageInfo();
     PermissionHandler()
         .checkPermissionStatus(PermissionGroup.storage)
         .then((hasPermission) async {
       if (hasPermission == PermissionStatus.granted) {
         try {
-          var dir = await getExternalStorageDirectory();
           var fileName = basename(url);
 
           // Create directory if not existing!
-          new Directory.fromUri(new Uri.file(dir.path + '/Knocky/'))
+          new Directory.fromUri(new Uri.file(dir[0].rootDir + '/Knocky/'))
               .createSync(recursive: true);
 
           Dio()
-              .download(url, dir.path + "/knocky/" + fileName)
+              .download(url, dir[0].rootDir + "/Knocky/" + fileName)
               .then((response) {
             if (response.statusCode == 200) {
               scaffoldKey.currentState.showSnackBar(new SnackBar(
@@ -36,11 +36,10 @@ class DownloadHelper {
           print(e);
         }
       } else {
-        var dir = await getApplicationDocumentsDirectory();
         var fileName = basename(url);
 
         // Create directory if not existing!
-        new Directory.fromUri(new Uri.file(dir.path + '/knocky/'))
+        new Directory.fromUri(new Uri.file(dir[0].rootDir + '/Knocky/'))
             .createSync(recursive: true);
 
         Map<PermissionGroup, PermissionStatus> permissions =
@@ -50,7 +49,7 @@ class DownloadHelper {
         if (permissions[PermissionGroup.storage] == PermissionStatus.granted) {
           Dio()
               .downloadUri(
-                  Uri.parse(url), dir.path + "/knocky/" + fileName)
+                  Uri.parse(url), dir[0].rootDir + "/Knocky/" + fileName)
               .then((response) {
             if (response.statusCode == 200) {
               scaffoldKey.currentState.showSnackBar(new SnackBar(
