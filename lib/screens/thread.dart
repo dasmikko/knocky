e66545dfbd29f932696429783600810be7c6cefd
Mathias <mathias_sn@hotmail.com>
2021-05-11 19:39:45 +0200

    added ScrollablePositionedList

--- lib/screens/thread.dart
+++ lib/screens/thread.dart
@@ -7,12 +7,14 @@ import 'package:knocky/models/thread.dart';
 import 'package:knocky/widgets/KnockoutLoadingIndicator.dart';
 import 'package:knocky/widgets/drawer/mainDrawer.dart';
 import 'package:knocky/widgets/post/postListItem.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ThreadScreen extends StatefulWidget {
   final int id;
   final int page;
+  final int linkedPostId;
 
-  ThreadScreen({@required this.id, this.page: 1});
+  ThreadScreen({@required this.id, this.page: 1, this.linkedPostId});
 
   @override
   _ThreadScreenState createState() => _ThreadScreenState();
@@ -43,7 +45,7 @@ class _ThreadScreenState extends State<ThreadScreen> {
               child: RefreshIndicator(
                   onRefresh: () async =>
                       threadController.fetchThreadPage(this.widget.page),
-                  child: ListView.builder(
+                  child: ScrollablePositionedList.builder(
                     itemCount: threadController.thread.value != null
                         ? threadController.thread.value.posts.length
                         : 0,
