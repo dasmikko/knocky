d33400e761d16a97eace5e9890962be7d1469688
dasmikko <dasmikko@gmail.com>
2021-06-08 18:03:47 +0200

    testing image fix

--- lib/widgets/post/postsElements/image.dart
+++ lib/widgets/post/postsElements/image.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:knocky/helpers/bbcodehelper.dart';
 import 'package:knocky/screens/imageViewer.dart';
+import 'package:measured_size/measured_size.dart';
 
 class ImageWidget extends StatefulWidget {
   final String url;
@@ -23,6 +24,8 @@ class _ImageWidgetState extends State<ImageWidget> {
     return [this.widget.url];
   }
 
+  Size loadedImageSize = Size.zero;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -39,18 +42,32 @@ class _ImageWidgetState extends State<ImageWidget> {
       },
       child: Hero(
         tag: this.widget.url + this.widget.postId.toString(),
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxHeight: 300),
-          child: CachedNetworkImage(
-            placeholder: (context, url) {
-              return Container(
-                height: 100,
-                width: 100,
-                padding: EdgeInsets.all(20),
-                child: CircularProgressIndicator(),
-              );
-            },
-            imageUrl: this.widget.url,
+        child: MeasuredSize(
+          onChange: (Size size) {
+            if (loadedImageSize == Size.zero && size != Size(100, 100)) {
+              print('Image loaded, setting image size!');
+              setState(() {
+                loadedImageSize = size;
+              });
+            } else {
+              print(loadedImageSize);
+            }
+          },
+          child: Container(
+            height:
+                loadedImageSize != Size.zero ? loadedImageSize.height : null,
+            width: loadedImageSize != Size.zero ? loadedImageSize.width : null,
+            child: CachedNetworkImage(
+              placeholder: (context, url) {
+                return Container(
+                  height: 100,
+                  width: 100,
+                  padding: EdgeInsets.all(20),
+                  child: CircularProgressIndicator(),
+                );
+              },
+              imageUrl: this.widget.url,
+            ),
           ),
         ),
       ),
