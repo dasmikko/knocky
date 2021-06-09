f04ad6465e5390514f9afed1d3d52f9e6d60d1ba
dasmikko <dasmikko@gmail.com>
2021-06-09 09:36:27 +0200

    new settings screen

---
+++ lib/screens/settings.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Reset image size cache'),
+              onTap: () {
+                final box = GetStorage('sizeCache');
+                box.erase();
+
+                Get.snackbar('Success', 'Image size cache cleared');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
