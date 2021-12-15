9f1b4b00455644263f048f8e76afb405431bee34
Mikkel Schuster Holm Jensen <mik@danak.dk>
2021-12-15 17:54:36 +0100

    fix android folder structure and windows test

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/mjensen/fvm/versions/2.8.0"
-export "FLUTTER_APPLICATION_PATH=/home/mjensen/repos/knocky"
+export "FLUTTER_ROOT=C:\Users\mik\fvm\versions\2.8.0"
+export "FLUTTER_APPLICATION_PATH=C:\repos\knocky"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.0"
-export "FLUTTER_BUILD_NUMBER=64"
+export "FLUTTER_BUILD_NAME=2.0.2"
+export "FLUTTER_BUILD_NUMBER=67"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
