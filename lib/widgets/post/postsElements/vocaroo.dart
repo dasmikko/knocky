e8c7e049df84be9e08560b88a0a52e5b20f3e670
dasmikko <dasmikko@gmail.com>
2021-12-16 21:03:33 +0100

    remove unused import

--- lib/widgets/post/postsElements/vocaroo.dart
+++ lib/widgets/post/postsElements/vocaroo.dart
@@ -1,7 +1,6 @@
 import 'package:better_player/better_player.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class VocarooEmbed extends StatelessWidget {
   final String url;
