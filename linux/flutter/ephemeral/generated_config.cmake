31d9f27fb4dd165f152f7fa6eb0c22de0e9db6dc
dasmikko <dasmikko@gmail.com>
2021-12-22 09:26:40 +0100

    force 2.8.1

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/mjensen/fvm/versions/2.8.1" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/mjensen/repos/knocky" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/mjensen/fvm/versions/2.8.1"
+  "PROJECT_DIR=/home/mjensen/repos/knocky"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/mjensen/repos/knocky/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/mjensen/repos/knocky/lib/main.dart"
+)
