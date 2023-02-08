d376aa1f1f5c2a604202ec198562752fc4633d31
dasmikko <dasmikko@gmail.com>
2023-02-08 19:28:20 +0100

    build stuff

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/mjensen/fvm/versions/3.7.0"
+export "FLUTTER_ROOT=/home/mjensen/fvm/versions/3.7.1"
 export "FLUTTER_APPLICATION_PATH=/home/mjensen/repos/knocky"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.2.6"
-export "FLUTTER_BUILD_NUMBER=79"
+export "FLUTTER_BUILD_NAME=2.3.0"
+export "FLUTTER_BUILD_NUMBER=80"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
